26a68b928e3d2ddb5482622337e0f8a26558a770
Claudio Godoy <clouds83@gmail.com>
2024-06-23 18:46:19 -0400

    schema draft

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,6 @@ import { Router, Request, Response } from 'express'
 const router = Router()
 
 router.get('/', (req: Request, res: Response) => {
-  throw new Error('Error message')
   return res.json({ message: 'Hello World' })
 })
 
