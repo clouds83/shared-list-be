45f351de3ebe1c45c56a15d3759c19ad5de6ef4e
Claudio Godoy <clouds83@gmail.com>
2024-06-25 22:41:39 -0400

    finished authenticate route

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,10 @@
 import { Router, Request, Response } from 'express'
 import { CreateUserController } from './controllers/user/CreateUserController'
+import { AuthUserController } from './controllers/user/AuthUserController'
 
 const router = Router()
 
 router.post('/create-user', new CreateUserController().handle)
+router.post('/authenticate', new AuthUserController().handle)
 
 export { router }
