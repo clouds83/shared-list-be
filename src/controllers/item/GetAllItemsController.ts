ed606edbd6192828269a37eb99cf7eb407fadc3a
Claudio Godoy <clouds83@gmail.com>
2024-07-20 20:15:31 -0400

    added route to get items + filter by category, pagination and sorting

--- src/controllers/item/GetAllItemsController.ts
+++ src/controllers/item/GetAllItemsController.ts
@@ -5,6 +5,11 @@ import prismaClient from '../../prisma'
 class GetAllItemsController {
   async handle(req: Request, res: Response) {
     const { user_id } = req
+    // page and pageSize from query parameters
+    const page = parseInt(req.query.page as string) || 1
+    const pageSize = parseInt(req.query.pageSize as string) || 10
+    const category = req.query.category as string
+    const sortOrder = (req.query.sortOrder as 'asc' | 'desc') || 'asc'
 
     const { subscriptionId } = await prismaClient.user.findUnique({
       where: {
@@ -17,7 +22,7 @@ class GetAllItemsController {
 
     const getAllItemsService = new GetAllItemsService()
 
-    const items = await getAllItemsService.execute(subscriptionId)
+    const items = await getAllItemsService.execute(subscriptionId, page, pageSize, category, sortOrder)
 
     return res.json(items)
   }
--- src/services/item/GetAllItemsService.ts
+++ src/services/item/GetAllItemsService.ts
@@ -1,14 +1,33 @@
 import prismaClient from '../../prisma'
 
 class GetAllItemsService {
-  async execute(subscriptionId: string) {
+  async execute(subscriptionId: string, page: number, pageSize: number, category: string, sortOrder: 'asc' | 'desc') {
     const items = await prismaClient.item.findMany({
       where: {
         subscriptionId,
+        category,
+      },
+      skip: (page - 1) * pageSize,
+      take: pageSize,
+      orderBy: {
+        name: sortOrder,
       },
     })
 
-    return items
+    // for pagination UI
+    const totalItems = await prismaClient.item.count({
+      where: {
+        subscriptionId,
+        category,
+      },
+    })
+
+    return {
+      items,
+      totalItems,
+      totalPages: Math.ceil(totalItems / pageSize),
+      currentPage: page,
+    }
   }
 }
 
